77ae53d1743284677da7552440039d51d0961af0
Doha09 <ouahmanedoha03@gmail.com>
2025-04-07 01:03:42 +0100

    Add files via upload

---
+++ web/src/java/test/TestMapped.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao2.ArticleDao;
+import dao2.CategorieDao;
+import dao2.CommentaireDao;
+import entities.Article;
+import entities.Categorie;
+import entities.Commentaire;
+import java.util.Date;
+
+/**
+ *
+ * @author ouahm
+ */
+public class TestMapped {
+
+    public static void main(String[] args) {
+
+        CategorieDao catDao = new CategorieDao();
+        catDao.create(new Categorie("Développement Web"));
+
+        Categorie categorie = catDao.findById(1);
+
+        ArticleDao articleDao = new ArticleDao();
+        Article article = new Article(
+                "Comprendre le CSS",
+                "Le CSS est un langage de style utilisé pour la mise en forme des pages web.",
+                new Date(),
+                categorie
+        );
+        articleDao.create(article);
+
+        CommentaireDao comDao = new CommentaireDao();
+        Commentaire commentaire = new Commentaire(
+                "Doha",
+                "Merci pour cet article très utile !",
+                article
+        );
+        comDao.create(commentaire);
+
+        System.out.println("✅ TestMapped exécuté avec succès !");
+    }
+}
