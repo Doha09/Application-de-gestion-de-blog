6e7c3d37dda543ade7919945fc7e2e236e488462
Doha09 <ouahmanedoha03@gmail.com>
2025-04-11 02:39:55 +0100

    Add files via upload

---
+++ web/src/java/dao2/AdminDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao2;
+
+import entities.Admin;
+
+/**
+ *
+ * @author ouahm
+ */
+public class AdminDao extends AbstractDao<Admin> {
+
+    public AdminDao() {
+        super(Admin.class);
+    }
+}
